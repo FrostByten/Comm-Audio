c15e27e600d3e2c184705a81e024e0bb410b1651
FrostByte <lewisscott0@gmail.com>
2015-02-19 08:43:49 -0800

    Created the server solution

---
+++ Source/server/server/server.cpp
@@ -0,0 +1,29 @@
+#include <Windows.h>
+#include <vlc/vlc.h>
+
+#pragma comment(lib, "libvlc.lib")
+#pragma comment(lib, "libvlccore.lib")
+
+int main(int argc, char* argv[])
+{
+	libvlc_instance_t *inst;
+	libvlc_media_player_t *mp;
+	libvlc_media_t *m;
+
+	inst = libvlc_new(0, NULL);
+
+	if (inst == NULL)
+		exit(0);
+	m = libvlc_media_new_location(inst, "http://incompetech.com/music/royalty-free/mp3-royaltyfree/Who%20Likes%20to%20Party.mp3");
+
+	if (m == NULL)
+		exit(0);
+	mp = libvlc_media_player_new_from_media(m);
+
+	libvlc_media_release(m);
+	libvlc_media_player_play(mp);
+
+	Sleep(5000);
+
+	return 0;
+}
