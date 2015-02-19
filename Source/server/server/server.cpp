7a41831e8bf6a97e1a51c8c44560a4de4a48e374
FrostByte <lewisscott0@gmail.com>
2015-02-19 12:02:39 -0800

    Updated server example

--- Source/server/server/server.cpp
+++ Source/server/server/server.cpp
@@ -10,20 +10,42 @@ int main(int argc, char* argv[])
 	libvlc_media_player_t *mp;
 	libvlc_media_t *m;
 
+	if (argc < 2)
+	{
+		MessageBox(NULL, "Need filename!", "Error", MB_OK | MB_ICONERROR);
+		exit(0);
+	}
+
 	inst = libvlc_new(0, NULL);
 
 	if (inst == NULL)
 		exit(0);
-	m = libvlc_media_new_path(inst, "dingdong.mp3");
 
+	m = libvlc_media_new_path(inst, argv[1]);
 	if (m == NULL)
+	{
+		MessageBox(NULL, "Unable to load media.", "Error", MB_OK | MB_ICONERROR);
 		exit(0);
-	mp = libvlc_media_player_new_from_media(m);
+	}
 
+	mp = libvlc_media_player_new_from_media(m);
 	libvlc_media_release(m);
 	libvlc_media_player_play(mp);
 
-	Sleep(500000);
+	//Wait for open media
+	Sleep(3000);
+
+	/* Wait until the file is finished */
+	libvlc_time_t time;
+	while (libvlc_media_player_is_playing(mp))
+	{
+		/* Print elapsed time */
+		time = libvlc_media_player_get_time(mp);
+		printf("|---%d ms elapsed---|\r", time);
+		Sleep(1);
+	}
+	OutputDebugString("finished?");
+	libvlc_media_player_release(mp);
 
 	return 0;
 }
